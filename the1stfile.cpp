108c3dfe118df4b44b68f187a85e42fba04f6f88
Orange Derek <158000262+WhosOrange@users.noreply.github.com>
2024-04-12 00:03:10 -0700

    Added new functions to the1stfile.cpp

--- the1stfile.cpp
+++ the1stfile.cpp
@@ -9,10 +9,29 @@ struct GoudaSticks
 
 GoudaSticks makeMeSomeSticks(int inAmt, std::string inSat) 
 {
-  GoudaStick johnny;
+  GoudaSticks johnny;
   johnny.amount = inAmt;
   johnny.satisfaction = inSat;
     return johnny; 
+};
+
+void refillGoudaSticks(GoudaSticks *inSticks, int inAmt) 
+{
+  inSticks->amount += inAmt;
+};
+
+std::string checkCase(std::string checkMe)
+{
+  int strLength = (int)checkMe.legnth(); //make sure that the length
+  for (int i = 0; i < strLength; i++)
+  {
+    // Check if is a letter, if not make it a space
+    if ( ((int)checkMe[i] < 65 && (int)checkMe[i] > 122 ) || ((int)checkMe[i] > 90 && (int)checkMe[i] < 97 )) {checkMe[i] = ' ';}
+    
+    // Check if is UPPERCASE, if so make lower
+    if ((int)checkMe[i] < (int) ) {checkMe[i] = (char)((int)(checkMe[i] + ));}
+  }
+  return checkMe;
 };
 
 int main()
@@ -28,10 +47,21 @@ int main()
   std::cout << "How is your satisfaction: ";
   std::getline(std::cin, youGood);
   
-  GoudaStick yourSticks = makeMeSomeSticks(howMany, youGood);
+  GoudaSticks yourSticks = makeMeSomeSticks(howMany, youGood);
 
   if (howMany == 0) {std::cout << "WELL I HOPE YOU TRULLY ARE " << yourSticks.satisfaction << " WITH YOUR LACK OF GOUDA STICKS\nYOU HEATHEN!\n";}
   else {std::cout << "I take great pleasure in that your " << yourSticks.amount << " have left you feeling " << yourSticks.satisfaction << ".\nGoodbye.\n";}
+
+  if (yourSticks.amount > 0)
+  {
+    std::cout << "Would you like to refill on Gouda Sticks?\n";
+    std::getline(std::cin, youGood);
+
+    if (youGood != checkCase("No") || youGood != checkCase("nah") || youGood != checkCase("maybe") || youGood != checkCase("no thank you"))
+    {
+      std::coud << "Maybe another time then.\n";
+    }
+  }
 
   std::getline(std::cin, youGood); //Just here for a brief pause
 
