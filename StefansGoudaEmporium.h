94928bc04eebaf39e1b6224c2ca21e3a043ed8ed
Orange Derek <158000262+WhosOrange@users.noreply.github.com>
2024-04-16 23:24:51 -0700

    Created Stefan's Gouda Emporium Header

---
+++ StefansGoudaEmporium.h
@@ -0,0 +1,15 @@
+// This is the header for Stefan's Gouda Emporium, the place of business that provides
+
+struct Date
+{
+  int day;
+  int year;
+} Date;
+
+struct SGE
+{
+  // Members!
+  int employees;  // Employee Count
+  Date established; // Date of Establishment
+  float rating;
+} SGE;
